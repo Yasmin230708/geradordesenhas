da6c6dc40f93c44f4f112959b0169c7f051cf6b3
Yasmin230708 <yasmin.negretti@escola.pr.gov.br>
2026-09-22 11:23:53 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -62,3 +62,57 @@ for(i=0; i < checkbox.length; i++){
     campoSenha.value = senha;
     classificaSenha(alfabeto.length);
     }
+
+    function classificaSenha(tamanhoAlfabeto) {
+
+
+        let entropia =
+    tamanhoSenha * Math.log2(tamanhoAlfabeto);
+    
+    
+       
+    console.log(entropia);
+    
+    
+       
+    forcaSenha.classList.remove('fraca', 'media', 'forte');
+    
+    
+        if (entropia >
+    57) {
+    
+    
+           
+    forcaSenha.classList.add('forte');
+    
+    
+        } else if
+    (entropia > 35 && entropia < 57) {
+    
+    
+           
+    forcaSenha.classList.add('media');
+    
+    
+        } else if
+    (entropia <= 35) {
+    
+    
+           
+    forcaSenha.classList.add('fraca');
+    
+    
+        }
+    
+    
+        const
+    valorEntropia = document.querySelector('.entropia');
+    
+    
+       
+    valorEntropia.textContent = "Um computador pode levar até " +
+    Math.floor(2 ** entropia / (100e6 * 60 * 60 * 24)) +
+    " dias para descobrir essa senha.";
+    
+    
+    }
